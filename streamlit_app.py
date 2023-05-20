b483bb0d1020899a24c8574d7968609c28c151ff
YourDataBro <133904778+YourDataBro@users.noreply.github.com>
2023-05-19 21:59:16 -0400

    Update streamlit_app.py
    Missing parenthesis when calling cursor()

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ def get_fruit_load_list():
     return my_cur.fetchall()
   
 def insert_row_snowflake(new_fruit):
-  with my_cnx.cursor as my_cur:
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES ('" + new_fruit + "')")
     return "Thanks for adding " + new_fruit
 
